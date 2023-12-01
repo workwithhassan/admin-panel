c31bb19e5ea8e1d3ef79f7046bc749c261b1f919
hassan <workwithhassan021@gmail.com>
2023-12-01 21:03:57 +0500

    Added table and created new page route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Sidebar from './shared/Sidebar';
-import Home from './components/Home';
-import Starred from './components/Starred';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Sidebar from "./shared/Sidebar";
+import Home from "./components/Home";
+import Starred from "./components/Starred";
+import Page from "./components/add-new/Page";
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
+    <div className="">
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/starred" element={<Starred />} />
+        </Routes>
+      </Sidebar>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/starred' element={<Starred />} />
+        <Route path="/new-page" element={<Page />} />
       </Routes>
     </div>
   );
--- src/shared/Sidebar.jsx
+++ src/shared/Sidebar.jsx
@@ -87,7 +87,7 @@ const Drawer = styled(MuiDrawer, {
   }),
 }));
 
-export default function Sidebar() {
+export default function Sidebar({children}) {
   const theme = useTheme();
   const navigate = useNavigate();
   const [open, setOpen] = React.useState(true);
@@ -101,9 +101,9 @@ export default function Sidebar() {
   };
 
   const handleNavigate = (link) => {
-    if (link == "Inbox") navigate('/')
-    if (link == "Starred") navigate('/starred')
-  }
+    if (link == "Inbox") navigate("/");
+    if (link == "Starred") navigate("/starred");
+  };
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -147,7 +147,7 @@ export default function Sidebar() {
                   justifyContent: open ? "initial" : "center",
                   px: 2.5,
                 }}
-                onClick={()=>handleNavigate(text)}
+                onClick={() => handleNavigate(text)}
               >
                 <ListItemIcon
                   sx={{
@@ -189,36 +189,7 @@ export default function Sidebar() {
           ))}
         </List>
       </Drawer>
-      {/* <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box> */}
+     {children}
     </Box>
   );
 }
