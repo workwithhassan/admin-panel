3eb3f72e6fe071e74b4761c5b3d789d232412841
hassan <workwithhassan021@gmail.com>
2023-12-01 18:49:17 +0500

    Created sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Sidebar from './shared/Sidebar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
     </div>
   );
 }
